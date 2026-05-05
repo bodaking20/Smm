788fffa33bd2f2bee31ffa9abf0e4843d4634f51
bodaking20 <41904926+bodaking20@users.noreply.github.com>
2026-05-05 21:35:56 +0300

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+def get_conn():
+    return psycopg2.connect(DATABASE_URL)
+
+@app.route("/")
+def home():
+    return "API is running"
+
+@app.route("/order", methods=["POST"])
+def order():
+    data = request.json
+
+    name = data.get("name")
+    phone = data.get("phone")
+    link = data.get("link")
+    quantity = data.get("quantity")
+    total = int(quantity) * 9
+
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute(\"\"\"
+        INSERT INTO orders (name, phone, link, quantity, total_price)
+        VALUES (%s, %s, %s, %s, %s)
+    \"\"\", (name, phone, link, quantity, total))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return jsonify({"status": "success"})
+
+
+@app.route("/admin")
+def admin():
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM orders ORDER BY id DESC")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(rows)
